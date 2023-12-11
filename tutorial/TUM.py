102616748ee5cb81a6cf6ad4d37f1257312d03b7
AlphaCentauri4 <94246993+AlphaCentauri4@users.noreply.github.com>
2023-12-11 18:16:38 -0500

    Corrected previous

--- tutorial/TUM.py
+++ tutorial/TUM.py
@@ -1,31 +1,49 @@
 ##############################################
 # Example of a 3D fluid simulation with phiflow - https://github.com/tum-pbs/PhiFlow
-# It creates a simple plume.
+# It creates a 3D TUM logo.
 # Author: Aristotelis
 # Date: 02 Aug 2021
-#############################################
-from phi.flow import *
-import time
+############################################# 
 
-#Params
-RES = 32
+from phi.flow import *
+import time 
+
+RES = 64
 RADIUS = 2
 T_STEP = 1
-DOMAIN = dict(x=RES, y=RES, z=RES)
-NUM_FRAMES = 200
+DOMAIN = dict(x=2*RES, y=RES, z=3*RES)
+NUM_FRAMES = 500
 
-#choose where to save the resulting frames
-SCENE_PATH = "/home/teemps/FILES/UNI/TUM/HiWi-Thurey/TUM_Logo/tum-logo/tutorial/test"
+#TODO: Change this path to where you want to save the simulation data
+SCENE_PATH = "sim_data"
 scene = Scene(SCENE_PATH)
 
-#The center's coordinates are in relation to the BOX not the resolution.
-INFLOW = CenteredGrid(Sphere(center=(8, 16, 16), radius=RADIUS), extrapolation.BOUNDARY, **DOMAIN) * 0.2
+
+OBSTACLE_GEOMETRIES = [
+    Box["x,y,z", 0:20*  RES           , 0:RES, 0.00     :0.20*  RES],
+    Box["x,y,z", 0:20*  RES           , 0:RES, 2.80* RES:3.00*  RES],
+    Box["x,y,z", 0:1.45*RES           , 0:RES, 0.20* RES:0.43*  RES],
+    Box["x,y,z", 0:0.30*RES           , 0:RES, 0.66* RES:2.56*  RES],
+    Box["x,y,z", 1.70*  RES:2*     RES, 0:RES, 0.20* RES:2.80*  RES],
+    Box["x,y,z", 0.30*  RES:1.45*  RES, 0:RES, 0.66* RES:0.90*  RES],
+    Box["x,y,z", 0.30*  RES:1.45*  RES, 0:RES, 1.70* RES:1.97*  RES],
+    Box["x,y,z", 0.30*  RES:1.45*  RES, 0:RES, 2.30* RES:2.55*  RES],
+    Box["x,y,z", 0.55*  RES:1.7*   RES, 0:RES, 1.15* RES:1.42*  RES]
+    
+]
+OBSTACLE = Obstacle(union(OBSTACLE_GEOMETRIES))
+OBSTACLE_MASK = HardGeometryMask(OBSTACLE.geometry) >> CenteredGrid(0, extrapolation.BOUNDARY, **DOMAIN)
+
+
+INFLOW = CenteredGrid(Box["x,y,z", 0.31*RES:0.45*RES, 0.35*RES:0.75*RES, 0.5* RES:0.65*RES ], extrapolation.BOUNDARY, **DOMAIN) * 0.2 + \
+         CenteredGrid(Box["x,y,z", 0.31*RES:0.45*RES, 0.35*RES:0.75*RES, 1.1* RES:1.5* RES ], extrapolation.BOUNDARY, **DOMAIN) * 0.2 + \
+         CenteredGrid(Box["x,y,z", 0.31*RES:0.45*RES, 0.35*RES:0.75*RES, 2.05*RES:2.2* RES ], extrapolation.BOUNDARY, **DOMAIN) * 0.2 + \
+         CenteredGrid(Box["x,y,z", 0.31*RES:0.45*RES, 0.35*RES:0.75*RES, 2.6* RES:2.75*RES ], extrapolation.BOUNDARY, **DOMAIN) * 0.2
 velocity = StaggeredGrid(Noise(scale=1), extrapolation.ZERO, **DOMAIN)  # or use CenteredGrid
 smoke = CenteredGrid(0, extrapolation.BOUNDARY, **DOMAIN)
 
 total_time = 0
 
-#Time stepping
 for step in range(NUM_FRAMES):
 
     start = time.time()
@@ -33,7 +51,7 @@ for step in range(NUM_FRAMES):
     buoyancy_force = smoke * (0.1, 0, 0) >> velocity  # resamples smoke to velocity sample points
     velocity = advect.semi_lagrangian(velocity, velocity, 1) + buoyancy_force
     with math.SolveTape() as solves:
-        velocity, pressure = fluid.make_incompressible(velocity, (), Solve('CG-adaptive', 1e-5, 0, x0=None))
+        velocity, pressure = fluid.make_incompressible(velocity, (OBSTACLE,), Solve('CG-adaptive', 1e-5, 0, max_iterations=1500, x0=None))
     scene.write({'smoke': smoke, 'velocity': velocity}, frame = step )
     end = time.time()
 
